bdc7e72387b04a73748fdb46ce3c964044c4c1e6
Ernest Bieś <ernestbies@gmail.com>
2023-02-22 15:13:35 +0100

    Backend URL update.

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -1,15 +1,17 @@
-const BACKEND_URL = 'https://ernestbies-backend.herokuapp.com';
+const BACKEND_URL = 'https://ernestbies-backend.onrender.com';
 
 export const fetchPosts = (signal) => {
-    return fetch(`${BACKEND_URL}/api/posts`, {signal}).then(response => response.json());
-}
+    return fetch(`${BACKEND_URL}/api/posts`, { signal }).then((response) =>
+        response.json()
+    );
+};
 
 export const sendPost = (data) => {
     return fetch(`${BACKEND_URL}/api/posts`, {
         method: 'post',
         headers: {
-            'Content-Type': 'application/json;charset=utf-8'
+            'Content-Type': 'application/json;charset=utf-8',
         },
-        body: JSON.stringify(data)
-    })
-}
+        body: JSON.stringify(data),
+    });
+};
