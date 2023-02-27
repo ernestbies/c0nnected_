909198ecfdfe1ebe91371acfb92d0d0aa3741489
Ernest Bieś <ernestbies@gmail.com>
2023-02-27 15:32:36 +0100

    Project info update.

--- src/components/ProjectIcon/LogoProgrammer/LogoProgrammer.js
+++ src/components/ProjectIcon/LogoProgrammer/LogoProgrammer.js
@@ -10,14 +10,14 @@ import {
 const LogoProgrammer = () => {
     return (
         <LogoContainer
-            href={'https://programm3r.netlify.app'}
-            title={'Programm3r - portfolio creator'}
+            href={'https://progr4mm3r.netlify.app'}
+            title={'Progr4mm3r - portfolio creator'}
             target={'_blank'}
             rel={'noreferrer'}
         >
             <StyledText>{'</>'}</StyledText>
             <TitleContainer>
-                <WebsiteTitle>{'Programm3r'}</WebsiteTitle>
+                <WebsiteTitle>{'Progr4mm3r'}</WebsiteTitle>
                 <WebsiteSubtitle>{'PORTFOLIO CREATOR'}</WebsiteSubtitle>
             </TitleContainer>
         </LogoContainer>
